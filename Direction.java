b57a89b7897192ac6777b467c380718035acc799
priyankakarale7 <110196022+priyankakarale7@users.noreply.github.com>
2023-02-26 08:35:31 +0530

    Add files via upload

---
+++ Direction.java
@@ -0,0 +1,29 @@
+
+/**
+ * The {@code Direction} enum is used to determine which way the Snake is moving.
+ * 
+ *
+ */
+public enum Direction {
+
+	/**
+	 * Moving North (Up).
+	 */
+	North,
+	
+	/**
+	 * Moving East (Right).
+	 */
+	East,
+	
+	/**
+	 * Moving South (Down).
+	 */
+	South,
+	
+	/**
+	 * Moving West (Left).
+	 */
+	West
+	
+}
---
+++ TileType.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * The { TileType} class represents the different
+ * types of tiles that can be displayed on the screen.
+ *
+ *
+ */
+public enum TileType {
+
+	Fruit,
+	
+	SnakeHead,
+	
+	SnakeBody
+	
+}
